feb3160fbf7d2c26d1b2a70c1ac429c3a1c55456
Tuki277 <huybeos2707@gmail.com>
2022-07-28 23:02:35 +0700

    fix google oauth

--- src/auth/strategy/google.strategy.ts
+++ src/auth/strategy/google.strategy.ts
@@ -15,6 +15,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
 
   async validate(
     accessToken: string,
+    refreshToken: string,
     profile: any,
     done: VerifyCallback,
   ): Promise<any> {
